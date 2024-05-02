79c76310121f5acf6da686123c79b7b398470c72
Chung Tu <tuthanhtrung2000@gmail.com>
2024-05-02 21:01:30 +0700

    fix build

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
  
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig =  {
+    webpack: (config) => {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@': path.resolve(__dirname, './'),
+      };
+  
+      return config;
+    },
+  };
  
 export default withNextIntl(nextConfig);
