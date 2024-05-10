95f737eb202b1117e2e4aead7efa6bdb4cf68277
Chung Tu <tuthanhtrung2000@gmail.com>
2024-05-10 16:49:17 +0700

    fix build

--- src/components/layout/DefaultLayout.tsx
+++ src/components/layout/DefaultLayout.tsx
@@ -1,9 +1,21 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "../header/Header";
 import Footer from "../footer/Footer";
+import { usePathname, useSearchParams } from "next/navigation";
+import NProgress from "nprogress";
 
 export default function DefaultLayout({ children }: any) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    NProgress.done();
+    console.log('aaa')
+    return () => {
+      NProgress.start();
+    };
+  }, [pathname, searchParams]);
   return (
     <div className="min-h-screen">
       <Header />
