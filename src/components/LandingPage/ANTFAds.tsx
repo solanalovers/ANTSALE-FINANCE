8960fc92bc1fc8b0b34eb7c19d57fea01a65a039
Chau Khac <chaukhac4@gmail.com>
2024-05-26 10:47:07 +0700

    feat: add logic to buy now

--- src/components/LandingPage/ANTFAds.tsx
+++ src/components/LandingPage/ANTFAds.tsx
@@ -1,20 +1,31 @@
-import { countdownToSaleEnd } from "@/function/timer";
-import { Button, Image, Input } from "@nextui-org/react";
-import { Copy } from "iconsax-react";
-import React, { ReactNode, useContext, useEffect, useState } from "react";
-import CustomDivider from "../CustomDivider";
-import useTrans from "@/hook/useTrans";
-import { useWallet } from "@solana/wallet-adapter-react";
-import { AppContext } from "@/provider/AppProvider";
-import { SaleType } from "@/interface/project-interface";
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { countdownToSaleEnd } from '@/function/timer';
+import { Button, Image, Input } from '@nextui-org/react';
+import { Copy } from 'iconsax-react';
+import React, { ReactNode, useContext, useEffect, useState } from 'react';
+import CustomDivider from '../CustomDivider';
+import useTrans from '@/hook/useTrans';
+import { useWallet } from '@solana/wallet-adapter-react';
+import { AppContext } from '@/provider/AppProvider';
+import { SaleType } from '@/interface/project-interface';
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import {
+  ComputeBudgetProgram,
+  Connection,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  TransactionMessage,
+  VersionedTransaction,
+} from '@solana/web3.js';
+import { getProgram } from '@/function/getProgram';
+import { BN } from '@coral-xyz/anchor';
+import { bs58 } from '@coral-xyz/anchor/dist/cjs/utils/bytes';
 
 const ContentBox = ({ children }: { children: ReactNode }) => (
   <div
-    className="bg-yellow-50 p-10 rounded-2xl"
+    className='bg-yellow-50 p-10 rounded-2xl'
     style={{
       boxShadow:
-        "0px 5px 15px -3px rgba(245,165,46,0.4), 0px 6px 4px -2px rgba(0,0,0,0.05)",
+        '0px 5px 15px -3px rgba(245,165,46,0.4), 0px 6px 4px -2px rgba(0,0,0,0.05)',
     }}
   >
     {children}
@@ -26,14 +37,111 @@ const MainContent = ({
 }: {
   content: { image: string; saleType: string; desc: string };
 }) => {
-  const t = useTrans("landing");
-  const t1 = useTrans("wallet");
+  const t = useTrans('landing');
+  const t1 = useTrans('wallet');
   const saleEndTime = new Date();
   saleEndTime.setHours(saleEndTime.getHours() + 17);
-  const [timer, setTimer] = useState("00:00:00:00");
-  const { publicKey } = useWallet();
+  const [timer, setTimer] = useState('00:00:00:00');
   const { balance } = useContext(AppContext);
   const [amount, setAmount] = useState<number>(0);
+
+  const [loading, setLoading] = useState(false);
+
+  const connection = new Connection(
+    process.env.NEXT_PUBLIC_HELIUS_RPC_DEVNET!,
+    'confirmed'
+  );
+  const wallet = useWallet();
+
+  const earlyBuy = async () => {
+    setLoading(true);
+    console.log('--Buying');
+
+    if (wallet && wallet.publicKey && wallet.signTransaction && amount > 0) {
+      try {
+        const program = getProgram(connection, wallet);
+
+        const buyIns = await program.methods
+          .invest(new BN(amount * LAMPORTS_PER_SOL))
+          .accounts({
+            investor: wallet.publicKey,
+            destination: new PublicKey(process.env.NEXT_PUBLIC_DESTINATION!),
+          })
+          .instruction();
+
+        const blockhash = await connection.getLatestBlockhashAndContext(
+          'confirmed'
+        );
+
+        const setComputeUnitPriceIx = ComputeBudgetProgram.setComputeUnitPrice({
+          microLamports: 100000,
+        });
+
+        const messageV0 = new TransactionMessage({
+          payerKey: wallet.publicKey,
+          recentBlockhash: blockhash.value.blockhash,
+          instructions: [setComputeUnitPriceIx, buyIns],
+        }).compileToV0Message();
+
+        const transactionV0 = new VersionedTransaction(messageV0);
+
+        const signature = await wallet.signTransaction(transactionV0);
+
+        const signatureEncode = bs58.encode(signature?.signatures?.[0]);
+
+        const blockHeight = await connection.getBlockHeight({
+          commitment: 'confirmed',
+          minContextSlot: blockhash.context.slot,
+        });
+
+        const transactionTTL = blockHeight + 151;
+        const waitToConfirm = () =>
+          new Promise((resolve) => setTimeout(resolve, 5000));
+        const waitToRetry = () =>
+          new Promise((resolve) => setTimeout(resolve, 2000));
+
+        const numTry = 30;
+
+        for (let i = 0; i < numTry; i++) {
+          // check transaction TTL
+          const blockHeight = await connection.getBlockHeight('confirmed');
+          if (blockHeight >= transactionTTL) {
+            throw new Error('ONCHAIN_TIMEOUT');
+          }
+
+          await connection.simulateTransaction(transactionV0, {
+            replaceRecentBlockhash: true,
+            commitment: 'confirmed',
+          });
+
+          await connection?.sendRawTransaction(signature.serialize(), {
+            skipPreflight: process.env.NODE_ENV === 'development',
+            maxRetries: 0,
+            preflightCommitment: 'confirmed',
+          });
+
+          await waitToConfirm();
+
+          const sigStatus = await connection.getSignatureStatus(
+            signatureEncode
+          );
+
+          if (sigStatus.value?.err) {
+            throw new Error('UNKNOWN_TRANSACTION');
+          }
+
+          if (sigStatus.value?.confirmationStatus === 'confirmed') {
+            break;
+          }
+
+          await waitToRetry();
+        }
+      } catch (error) {
+        console.log('Send transaction error: ', error);
+      }
+    }
+    setLoading(false);
+  };
 
   useEffect(() => {
     const countdown = setInterval(() => {
@@ -43,167 +151,170 @@ const MainContent = ({
     return () => clearInterval(countdown);
   }, []);
   return (
-    <div className="flex flex-col items-center justify-center gap-y-4">
+    <div className='flex flex-col items-center justify-center gap-y-4'>
       <Image
         src={content.image}
-        className="w-[60px] h-[60px] object-cover object-center"
+        className='w-[60px] h-[60px] object-cover object-center'
       />
-      <p className="text-[20px] leading-[34px] font-semibold text-[#1C1C1E] text-center">
-        $ANTF <span className="font-bold">{content.saleType} Sale</span>
+      <p className='text-[20px] leading-[34px] font-semibold text-[#1C1C1E] text-center'>
+        $ANTF <span className='font-bold'>{content.saleType} Sale</span>
       </p>
-      <p className="text-center text-sm leading-[22px]">{content.desc}</p>
-      <div className="flex items-center gap-x-1">
-        <p className="text-[13px] leading-[32px] text-[#1C1C1E] underline">
+      <p className='text-center text-sm leading-[22px]'>{content.desc}</p>
+      <div className='flex items-center gap-x-1'>
+        <p className='text-[13px] leading-[32px] text-[#1C1C1E] underline'>
           9RFFhhe4XPV8UcBFJkgrDwGGtN3jmktBtw4RBia1bBVn
         </p>
         <Copy
-          className="hover:opacity-80 cursor-pointer"
-          variant="Bold"
+          className='hover:opacity-80 cursor-pointer'
+          variant='Bold'
           size={20}
-          color="#006FEE"
+          color='#006FEE'
           onClick={() =>
             navigator.clipboard.writeText(
-              "9RFFhhe4XPV8UcBFJkgrDwGGtN3jmktBtw4RBia1bBVn"
+              '9RFFhhe4XPV8UcBFJkgrDwGGtN3jmktBtw4RBia1bBVn'
             )
           }
         />
       </div>
-      <div className="flex items-center gap-x-4 w-full">
+      <div className='flex items-center gap-x-4 w-full'>
         <Input
-          placeholder="0"
-          label={`${t("ads.amount")} ${
-            publicKey ? `(Your balances: ${balance} SOL)` : ""
+          placeholder='0'
+          label={`${t('ads.amount')} ${
+            wallet.publicKey ? `(Your balances: ${balance} SOL)` : ''
           }`}
           onChange={(e) => {
             if (!e.target.value || !Number.isNaN(Number(e.target.value))) {
               setAmount(Number(e.target.value));
             } else {
-              e.target.value = "";
+              e.target.value = '';
             }
           }}
           value={amount?.toString()}
           endContent={
             <>
-              {publicKey && (
+              {wallet.publicKey && (
                 <p
-                  className="absolute right-2 cursor-pointer"
+                  className='absolute right-2 cursor-pointer'
                   onClick={() => {
-                    if (publicKey) {
+                    if (wallet.publicKey) {
                       setAmount(balance);
                     }
                   }}
                 >
-                  {t("ads.max")}
+                  {t('ads.max')}
                 </p>
               )}
             </>
           }
-          size="sm"
-          variant="flat"
-          color="default"
-          classNames={{ inputWrapper: "bg-white" }}
+          size='sm'
+          variant='flat'
+          color='default'
+          classNames={{ inputWrapper: 'bg-white' }}
         />
-        <div className="w-[40%]">
+        <div className='w-[40%]'>
           <div
-            color="primary"
-            className="w-full font-medium text-base leading-6 h-12 px-4 bg-primary relative hover:opacity-50 rounded-lg flex items-center justify-center text-white"
+            color='primary'
+            className='w-full font-medium text-base leading-6 h-12 px-4 bg-primary relative hover:opacity-50 rounded-lg flex items-center justify-center text-white'
+            onClick={async () => {
+              await earlyBuy();
+            }}
           >
-            {!publicKey && <WalletMultiButton />}
-            {t("ads.buy")}
+            {!wallet.publicKey && <WalletMultiButton />}
+            {t('ads.buy')}
           </div>
         </div>
       </div>
-      <p className="text-[20px] leading-[28px] font-semibold text-[#1C1C1E]">
-        {t("ads.saleStart")}{" "}
-        <span className="font-bold text-primary">{timer}</span>
+      <p className='text-[20px] leading-[28px] font-semibold text-[#1C1C1E]'>
+        {t('ads.saleStart')}{' '}
+        <span className='font-bold text-primary'>{timer}</span>
       </p>
     </div>
   );
 };
 
 const Divider = () => (
-  <div className="my-8">
+  <div className='my-8'>
     <svg
-      width="100%"
-      height="1"
-      viewBox="0 0 100% 1"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
+      width='100%'
+      height='1'
+      viewBox='0 0 100% 1'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
     >
       <line
-        y1="0.5"
-        x2="100%"
-        y2="0.5"
-        stroke="#66AAF9"
-        stroke-dasharray="12 12"
+        y1='0.5'
+        x2='100%'
+        y2='0.5'
+        stroke='#66AAF9'
+        stroke-dasharray='12 12'
       />
     </svg>
   </div>
 );
 
 export default function ANTFAds() {
-  const t = useTrans("landing");
+  const t = useTrans('landing');
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+    <div className='grid grid-cols-1 lg:grid-cols-2 gap-8'>
       <ContentBox>
         <MainContent
           content={{
-            saleType: t("ads.seed"),
-            image: "/image/landing/seed.png",
-            desc: t("ads.seedDesc"),
+            saleType: t('ads.seed'),
+            image: '/image/landing/seed.png',
+            desc: t('ads.seedDesc'),
           }}
         />
         <Divider />
         <div>
-          <p className="text-[36px] leading-[42px] font-semibold">
-            $ANTF {t("ads.token")}
+          <p className='text-[36px] leading-[42px] font-semibold'>
+            $ANTF {t('ads.token')}
           </p>
-          <ul className="list-disc ml-4 mt-6">
+          <ul className='list-disc ml-4 mt-6'>
             <li>
-              <p className="text-base leading-6">
-                <span className="font-medium">{t("ads.seedSale")}:</span>{" "}
+              <p className='text-base leading-6'>
+                <span className='font-medium'>{t('ads.seedSale')}:</span>{' '}
                 100,000 ANTF (10%)
               </p>
             </li>
             <li>
-              <p className="text-base leading-6">
-                <span className="font-medium">{t("ads.fairlaunchSale")}:</span>{" "}
+              <p className='text-base leading-6'>
+                <span className='font-medium'>{t('ads.fairlaunchSale')}:</span>{' '}
                 500,000 ANTF (50%)
               </p>
             </li>
             <li>
-              <p className="text-base leading-6">
-                <span className="font-medium">{t("ads.liquidity")}:</span>{" "}
+              <p className='text-base leading-6'>
+                <span className='font-medium'>{t('ads.liquidity')}:</span>{' '}
                 200,000 ANTF (20%)
               </p>
             </li>
             <li>
-              <p className="text-base leading-6">
-                <span className="font-medium">{t("ads.cex")}:</span> 100,000
+              <p className='text-base leading-6'>
+                <span className='font-medium'>{t('ads.cex')}:</span> 100,000
                 ANTF (10%)
               </p>
             </li>
             <li>
-              <p className="text-base leading-6">
-                <span className="font-medium">{t("ads.dev")}:</span> 50,000 ANTF
+              <p className='text-base leading-6'>
+                <span className='font-medium'>{t('ads.dev')}:</span> 50,000 ANTF
                 (5%)
               </p>
             </li>
             <li>
-              <p className="text-base leading-6">
-                <span className="font-medium">{t("ads.airdrop")}:</span> 50,000
+              <p className='text-base leading-6'>
+                <span className='font-medium'>{t('ads.airdrop')}:</span> 50,000
                 ANTF (5%)
               </p>
             </li>
             <li>
-              <p className="text-base leading-6">
-                <span className="font-medium">{t("ads.total")}:</span> 1,000,000
+              <p className='text-base leading-6'>
+                <span className='font-medium'>{t('ads.total')}:</span> 1,000,000
                 ANTF
               </p>
             </li>
             <li>
-              <p className="text-base leading-6">
-                <span className="font-medium">{t("ads.maxSupply")}:</span>{" "}
+              <p className='text-base leading-6'>
+                <span className='font-medium'>{t('ads.maxSupply')}:</span>{' '}
                 1,000,000 ANTF
               </p>
             </li>
@@ -211,43 +322,43 @@ export default function ANTFAds() {
         </div>
       </ContentBox>
       <ContentBox>
-        <div className="flex flex-col gap-y-6">
-          <p className="text-[36px] leading-[42px] text-[#1C1C1E] font-semibold">
-            {t("ads.howtobuy")} SOL
+        <div className='flex flex-col gap-y-6'>
+          <p className='text-[36px] leading-[42px] text-[#1C1C1E] font-semibold'>
+            {t('ads.howtobuy')} SOL
           </p>
-          <ul className="list-disc ml-4 text-base text-[#1C1C1E] leading-6">
-            <li>{t("ads.faq1")}</li>
-            <li>{t("ads.faq2")}</li>
-            <li>{t("ads.faq3")}</li>
+          <ul className='list-disc ml-4 text-base text-[#1C1C1E] leading-6'>
+            <li>{t('ads.faq1')}</li>
+            <li>{t('ads.faq2')}</li>
+            <li>{t('ads.faq3')}</li>
           </ul>
           <div>
-            <p>{t("ads.fairContent1")}</p>
-            <p className="text-base leading-6 text-[#1C1C1E]">
-              <span className="text-primary underline">
+            <p>{t('ads.fairContent1')}</p>
+            <p className='text-base leading-6 text-[#1C1C1E]'>
+              <span className='text-primary underline'>
                 9RFFhhe4XPV8UcBFJkgrDwGGtN3jmktBtw4RBia1bBVn
               </span>
               {` `}
               <Copy
-                className="hover:opacity-80 cursor-pointer inline"
-                variant="Bold"
+                className='hover:opacity-80 cursor-pointer inline'
+                variant='Bold'
                 size={20}
-                color="#006FEE"
+                color='#006FEE'
                 onClick={() =>
                   navigator.clipboard.writeText(
-                    "9RFFhhe4XPV8UcBFJkgrDwGGtN3jmktBtw4RBia1bBVn"
+                    '9RFFhhe4XPV8UcBFJkgrDwGGtN3jmktBtw4RBia1bBVn'
                   )
                 }
               />
-              {t("ads.fairContent2")}
+              {t('ads.fairContent2')}
             </p>
           </div>
         </div>
         <Divider />
         <MainContent
           content={{
-            saleType: t("ads.fairlaunch"),
-            image: "/image/landing/fairlaunch.png",
-            desc: t("ads.fairlaunchDesc"),
+            saleType: t('ads.fairlaunch'),
+            image: '/image/landing/fairlaunch.png',
+            desc: t('ads.fairlaunchDesc'),
           }}
         />
       </ContentBox>
---
+++ src/function/getProgram.ts
@@ -0,0 +1,17 @@
+import { Connection, PublicKey } from '@solana/web3.js';
+import { AnchorProvider, Program } from '@coral-xyz/anchor';
+import { AntsaleContract } from '@/anchor/antsale_contract';
+import IDL from '@/anchor/antsale_contract.json';
+
+export const getProgram = (connection: Connection, wallet: any) => {
+  const provider = new AnchorProvider(connection, wallet, {
+    commitment: 'confirmed',
+  });
+
+  const program = new Program<AntsaleContract>(
+    IDL as AntsaleContract,
+    provider
+  );
+
+  return program;
+};
