5c29354a275ddbaf25ced39ccfc786f4f054a0b1
Chung Tu <tuthanhtrung2000@gmail.com>
2024-05-28 14:49:12 +0700

    update layout

--- src/components/layout/DefaultLayout.tsx
+++ src/components/layout/DefaultLayout.tsx
@@ -5,6 +5,7 @@ import Footer from "../footer/Footer";
 import { usePathname, useSearchParams } from "next/navigation";
 import NProgress from "nprogress";
 import AdsItem from "../Ads/AdsItem";
+import useTrans from "@/hook/useTrans";
 
 export default function DefaultLayout({ children }: any) {
   const pathname = usePathname();
@@ -16,9 +17,14 @@ export default function DefaultLayout({ children }: any) {
       NProgress.start();
     };
   }, [pathname, searchParams]);
+  const tAnouncement = useTrans('anouncement')
   return (
     <div className="min-h-screen">
-      {pathname.split("/").length === 2 && <AdsItem content={`Introducing: <span class='font-semibold text-primary-500'>$ANTF Seed Sale!</span> Once-in-a-lifetime opportunity in the blockchain space, don't miss out`} />}
+      <AdsItem content={
+        pathname.split("/").length === 2 ?
+        tAnouncement('home')
+      : tAnouncement('other')
+      } />
       <Header />
       {children}
       <Footer />
