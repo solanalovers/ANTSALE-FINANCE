da2a9e7ea834d1b0520366133751e66362b03b77
Chung Tu <tuthanhtrung2000@gmail.com>
2024-05-29 09:38:00 +0700

    change position create token

--- src/components/create-token/CreateForm.tsx
+++ src/components/create-token/CreateForm.tsx
@@ -122,7 +122,7 @@ export default function CreateForm() {
         listStep={listStep}
         step={1}
         active
-        type='createToken'
+        type="createToken"
       />
       <CustomDivider />
       <RadioGroup
@@ -198,6 +198,24 @@ export default function CreateForm() {
                 }))
               }
             />
+            <Checkbox className="items-start gap-2">
+              <p className="text-base leading-[140%]">
+                Are you going to Create a Liquidity Pool?
+              </p>
+              <p className="text-xs leading-[140%] text-[#718096]">
+                {`If you want to create a liquidity pool you will need to "Revoke
+                Freeze Authority" of the Token, you can do that here. The cost
+                is FREE.`}
+              </p>
+            </Checkbox>
+            <Checkbox className="items-start gap-2">
+              <p className="text-base leading-[140%]">Revoke Mint Authority</p>
+              <p className="text-xs leading-[140%] text-[#718096]">
+                {`Revoking mint authority ensures that there can be no more tokens
+                minted than the total supply. This provides security and peace
+                of mind to buyers. The cost is FREE.`}
+              </p>
+            </Checkbox>
           </div>
           {currentEdit === "token-2022" && (
             <div>
@@ -257,26 +275,6 @@ export default function CreateForm() {
                     }))
                   }
                 />
-                <Checkbox className="items-start gap-2">
-                  <p className="text-base leading-[140%]">
-                    Are you going to Create a Liquidity Pool?
-                  </p>
-                  <p className="text-xs leading-[140%] text-[#718096]">
-                    {`If you want to create a liquidity pool you will need to "Revoke
-                Freeze Authority" of the Token, you can do that here. The cost
-                is FREE.`}
-                  </p>
-                </Checkbox>
-                <Checkbox className="items-start gap-2">
-                  <p className="text-base leading-[140%]">
-                    Revoke Mint Authority
-                  </p>
-                  <p className="text-xs leading-[140%] text-[#718096]">
-                    {`Revoking mint authority ensures that there can be no more tokens
-                minted than the total supply. This provides security and peace
-                of mind to buyers. The cost is FREE.`}
-                  </p>
-                </Checkbox>
               </div>
             </div>
           )}
