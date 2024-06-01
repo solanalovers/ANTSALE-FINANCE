96017822302bc7e54ee8ddae3ba9395042b0fa69
Chung Tu <tuthanhtrung2000@gmail.com>
2024-06-01 10:39:14 +0700

    update trans

--- src/constant/localeItemConstant.ts
+++ src/constant/localeItemConstant.ts
@@ -9,4 +9,24 @@ export const localeList = [
         value: 'kr',
         icon: '/image/locale/kr.png'
     },
+    {
+        label: 'China',
+        value: 'cn',
+        icon: '/image/locale/kr.png'
+    },
+    {
+        label: 'Japan',
+        value: 'jp',
+        icon: '/image/locale/jp.png'
+    },
+    {
+        label: 'Vietnam',
+        value: 'vn',
+        icon: '/image/locale/vn.png'
+    },
+    {
+        label: 'Russia',
+        value: 'ru',
+        icon: '/image/locale/ru.png'
+    },
 ]
