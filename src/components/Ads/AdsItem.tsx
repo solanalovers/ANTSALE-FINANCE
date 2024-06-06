f588b55859372b673e6ef3446f7f3f7ffba0d69f
Chung Tu <tuthanhtrung2000@gmail.com>
2024-06-06 22:07:57 +0700

    #3

--- src/components/Ads/AdsItem.tsx
+++ src/components/Ads/AdsItem.tsx
@@ -4,7 +4,7 @@ export default function AdsItem({ content }: { content: string }) {
   return (
     <div className="p-4 flex items-center justify-center bg-primary-50">
       <p
-        className="text-[18px] leading-[26px] text-primary-300"
+        className="text-[16px] leading-[24px] text-primary-300"
         dangerouslySetInnerHTML={{ __html: content }}
       ></p>
     </div>
